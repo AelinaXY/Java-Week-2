1f6317b99c69709c4a3c5ccff5a54dadaf47be3a
AelinaXY <Lauren@2infinity.net>
2023-03-15 12:32:31 +0000

    Add getters andd setters to Person

--- src/main/Person.java
+++ src/main/Person.java
@@ -12,5 +12,17 @@ public class Person {
 	public void stringOutput() {
 		System.out.println("My name is " + name + " and I am " + age + " years old. I am a " + title);
 	}
+	
+	
+	public void setAge (int age) this.age = age;
+	public void setName (int name) this.name = name;
+	public void setTitle (int title) this.title = title;
+	
+	public int getAge() return age;
+	public String getName() return name;
+	public String getTitle() return title;
+
+
+	
 
 }
